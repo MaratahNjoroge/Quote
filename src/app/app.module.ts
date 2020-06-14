8c0b625c0fe0cfb0d4d82f1702680f429ceb0a01
Maratah <maratah7@gmail.com>
2020-06-14 08:52:57 +0300

    add input to the file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QoutesComponent } from './qoutes/qoutes.component';
+import { QoutesComponent} from './qoutes/qoutes.component'
+import { QuotesModule } from './qoutes/QoutesModule';
 
 @NgModule({
   declarations: [
     AppComponent,
     QoutesComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QuotesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
